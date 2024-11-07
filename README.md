295d5121d844c64244f78def31def8bded0c2ffa
Paul Riddell <paul.riddell@space-bx.co.uk>
2024-11-07 11:01:51 +0000

    finished

--- README.md
+++ README.md
@@ -1,8 +1,5 @@
-# React + Vite
+# Meme Generator
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
-
-Currently, two official plugins are available:
-
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+- utilize useEffect()
+- hook into web api to get images
+- utilize state to keep images in memory and dynamically show text on the image
--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,29 +1,68 @@
-import memeData from "../memesdata.js";
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 export default function Meme() {
 
-    const [memeImage, setMemeImage] = useState("")
+    const [meme, setMeme] = useState(
+        {
+            topText: "",
+            bottomText: "",
+            randomImage: "http://i.imgflip.com/1bij.jpg"
+        }
+    )
+    const [allMemes, setallMemes] = useState([])
+
+    useEffect(() => {
+
+        fetch("https://api.imgflip.com/get_memes")
+            .then(response => response.json())
+            .then(data => setallMemes(data.data.memes))
+
+        return () => {
+            console.log("Component unmounted")
+        }
+    }, [])
 
     function getMemeImage() {
-        console.log("i was clicked")
+        const randomNumber = Math.floor(Math.random() * allMemes.length)
+        const url = allMemes[randomNumber].url
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            randomImage: url
+        }))
+    }
 
-        const memesArray = memeData.data.memes
-        const randomNumber = Math.floor(Math.random() * memesArray.length)
-        setMemeImage(memesArray[randomNumber].url)
-        console.log(memesArray[randomNumber].url)
-
-
+    function handleChange(event) {
+        const { name, value } = event.target
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            [name]: value
+        }))
     }
 
     return (
         <main>
             <div className="form">
-                <input type="text" className="form--input" placeholder="Top text" />
-                <input type="text" className="form--input" placeholder="Bottom text" />
+                <input
+                    type="text"
+                    className="form--input"
+                    placeholder="Top text"
+                    name="topText"
+                    onChange={handleChange}
+                    value={meme.topText} />
+                <input
+                    type="text"
+                    className="form--input"
+                    placeholder="Bottom text"
+                    name="bottomText"
+                    onChange={handleChange}
+                    value={meme.bottomText} />
                 <button onClick={getMemeImage} className="form--button">Get a new meme image</button>
             </div>
-            <img src={memeImage} alt="meme" className="meme--image" />
+            <div className="meme">
+                <img src={meme.randomImage} className="meme--image" />
+                <h2 className="meme--text top">{meme.topText}</h2>
+                <h2 className="meme--text bottom">{meme.bottomText}</h2>
+            </div>
         </main>
     )
 }
